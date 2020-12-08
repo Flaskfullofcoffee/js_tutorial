33696584ee7c8da0ba901746f0088deb706859fb
Flaskfullofcoffee <zdobbs3@gmail.com>
2020-12-08 09:56:22 -0700

    test

--- day.js
+++ day.js
@@ -1,3 +1,5 @@
+let now = new Date();
+alert(`Happy ${dayName(now)}!`);
 let dayName = (date) => {
   const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday',
                         'Wednesday', 'Thursday', 'Friday', 'Saturday'];
---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// let now = new Date();
+// alert(`Happy ${dayName(now)}!`);
+// let dayName = (date) => {
+//   let today = '';
+//   const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday',
+//                         'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+//   return daysOfTheWeek[date..getDay()];
+// }
+// let today =
+
+let changeColor = (newColor) => {
+  let ele = document.getElementById('para');
+  ele.style.color = newColor;
+}
