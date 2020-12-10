f2a9db1042515bc4e4d860f9459bbe96669359e5
Flaskfullofcoffee <zdobbs3@gmail.com>
2020-12-10 08:45:58 -0700

    wipe

--- day.js
+++
@@ -1,7 +0,0 @@
-let now = new Date();
-alert(`Happy ${dayName(now)}!`);
-let dayName = (date) => {
-  const daysOfTheWeek = ['Sunday', 'Monday', 'Tuesday',
-                        'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-  return daysOfTheWeek[date..getDay()];
-}
